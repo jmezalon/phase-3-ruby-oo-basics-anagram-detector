7f29166e25d084ac8feb54ad385a1580f36f5087
Max Mezalon <jmezalon@gmail.com>
2021-11-23 11:55:11 -0500

    commit tree 6ba0487bd0b6175b542723ea04559b8b0464fe7f

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+
+    def match(wordArray)
+        @wordArray = wordArray
+        
+        @result = []
+        @wordArray.filter do |w|
+            if @word.split('').sort == w.split('').sort
+                @result << w 
+            end
+        end
+        @result
+    end
+end
+
+
+
